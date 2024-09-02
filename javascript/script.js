19a76a07dfdf5d47cfe4e67bf8a26339f5e5d323
rutang2406 <155040265+rutang2406@users.noreply.github.com>
2024-09-02 18:23:28 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -6,22 +6,24 @@ let transformed=true;
 change.onclick =function() {
     if(transformed){
     container.style.transform = "translateX(-88%)";
-    change.innerHTML="SIGN IN";
+    setTimeout(() => {
+        change.innerHTML="SIGN IN";
     heading.innerHTML="Welcome Back!"
     heading.style.marginTop="120px";
     paragraph.innerHTML="Enter your personal to use this site"
     paragraph.style.marginLeft="50px";
     change.style.marginLeft="120px"
     transformed=false;
+    }, 950);
     }
     else{
         container.style.transform = "translateX(-184.5%)";
-    change.innerHTML="SIGN UP";
-    heading.innerHTML="Hello, Friend!"
-    heading.style.marginTop="25%";
-    paragraph.innerHTML="Register with your personal details to use all site features"
-    paragraph.style.marginLeft="20px";
-    change.style.marginLeft="100px";
-    transformed=true;
+        setTimeout(()=>{change.innerHTML="SIGN UP";
+        heading.innerHTML="Hello, Friend!"
+        heading.style.marginTop="25%";
+        paragraph.innerHTML="Register with your personal details to use site"
+        paragraph.style.marginLeft="20px";
+        change.style.marginLeft="100px";
+        transformed=true;},950)
     }
 };
